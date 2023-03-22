05f81092b8bc84b242601e6cd1ca4bbd26e1b41f
Javier Palacios <javier.palacios.h@gmail.com>
2023-03-22 02:11:10 +0100

    create new banner components and create fixed header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Header from './components/header'
-
-import "./App.css";
+import Header from "./components/header";
+import EventsBanner from "./components/banner-events";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <EventsBanner />
     </div>
   );
 }
