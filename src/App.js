6f41e58ff7c621203ae2f24efea7c2127858393e
Javier Palacios <javier.palacios.h@gmail.com>
2023-03-22 01:42:04 +0100

    create new header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
+import Header from './components/header'
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div>hola</div>
+      <Header />
     </div>
   );
 }
