a16f06715a91c2e16f103883fc20ca930c02ca68
Sergio Pizarro <sergio.pizarroseg@gmail.com>
2023-03-28 01:53:57 +0200

    created routes system

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,8 +5,16 @@ import Icon from "../../assets/images/icon_no_bg.png";
 function Header() {
   const menuItems = (
     <>
-      <li className="menu-item">home</li>
-      <li className="menu-item">somos</li>
+      <li>
+        <a href="/" className="menu-item">
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="/about" className="menu-item">
+          About us
+        </a>
+      </li>
       <li className="menu-item">releases</li>
       <li className="menu-item">videos</li>
       <li className="menu-item">bootlegger</li>
@@ -16,7 +24,7 @@ function Header() {
 
   return (
     <header className="header">
-      <img className="menu-logo" src={Icon} alt="menu-logo" />
+      <img href="/" className="menu-logo" src={Icon} alt="logo" />
       <nav data-state="desktop">
         <ul className="menu-desktop">{menuItems}</ul>
       </nav>
---
+++ src/pages/home/index.js
@@ -0,0 +1,15 @@
+// import logo from "./logo.svg";
+import BannerEvents from "../../components/banner-events";
+import Header from "../../components/header";
+import "./home.css";
+
+function Home() {
+  return (
+    <div className="App">
+      <Header />
+      <BannerEvents />
+    </div>
+  );
+}
+
+export default Home;
