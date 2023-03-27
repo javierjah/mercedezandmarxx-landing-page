83464cd1aaef86a85f821764f2a310952baeedc8
nicowave <c.nicofuentes@gmail.com>
2023-03-28 00:48:20 +0200

    dont do nothing, lasen sie alles so bitte

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/header";
 import EventsBanner from "./components/banner-events";
 import NewAlbum from "./components/new-album";
+import Tweets from "./components/tweets/img-tweets";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <EventsBanner />
       <NewAlbum />
+      <Tweets />
     </div>
   );
 }
