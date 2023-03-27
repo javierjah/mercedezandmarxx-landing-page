ef8b297672efb650770e2d60fe7caac572d9854e
Sergio Pizarro <sergio.pizarroseg@gmail.com>
2023-03-28 00:34:47 +0200

    hermanito i am very sorry for the bleed in your eyes that the code gave you

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,17 +1,17 @@
 // import logo from "./logo.svg";
 import "./header.css";
-import Icon from "../../assets/images/icon_no_bg.png"
+import Icon from "../../assets/images/icon_no_bg.png";
 
 function Header() {
   return (
     <header className="header">
-    <img className="menu-logo" src={Icon} alt="" />
-    <div className="menu-item">home</div>
-    <div className="menu-item">somos</div>
-    <div className="menu-item">releases</div>
-    <div className="menu-item">videos</div>
-    <div className="menu-item">bootlegger</div>
-    <div className="menu-item">shop</div>
+      <img className="menu-logo" src={Icon} alt="" />
+      <div className="menu-item">home</div>
+      <div className="menu-item">somos</div>
+      <div className="menu-item">releases</div>
+      <div className="menu-item">videos</div>
+      <div className="menu-item">bootlegger</div>
+      <div className="menu-item">shop</div>
     </header>
   );
 }
