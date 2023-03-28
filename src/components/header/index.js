755fcf02f5166c74536411ae9f5b0563cddacd11
Javier Palacios <javier.palacios.h@gmail.com>
2023-03-28 03:10:29 +0200

    feature: add responsive header behaviour

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,18 +1,31 @@
-// import logo from "./logo.svg";
+import MenuIcon from "@mui/icons-material/Menu";
 import "./header.css";
 import Icon from "../../assets/images/icon_no_bg.png";
 
 function Header() {
+  const menuItems = (
+    <>
+      <li className="menu-item">home</li>
+      <li className="menu-item">somos</li>
+      <li className="menu-item">releases</li>
+      <li className="menu-item">videos</li>
+      <li className="menu-item">bootlegger</li>
+      <li className="menu-item">shop</li>
+    </>
+  );
+
   return (
     <header className="header">
-
-      <img className="menu-logo" src={Icon} alt="" />
-      <div className="menu-item">home</div>
-      <div className="menu-item">somos</div>
-      <div className="menu-item">releases</div>
-      <div className="menu-item">videos</div>
-      <div className="menu-item">bootlegger</div>
-      <div className="menu-item">shop</div>
+      <img className="menu-logo" src={Icon} alt="menu-logo" />
+      <nav data-state="desktop">
+        <ul className="menu-desktop">{menuItems}</ul>
+      </nav>
+      <nav data-state="mobile">
+        <div className="button-items-box">
+          <MenuIcon fontSize="large" className="menu-icon" />
+          <ul className="menu-mobile">{menuItems}</ul>
+        </div>
+      </nav>
     </header>
   );
 }
