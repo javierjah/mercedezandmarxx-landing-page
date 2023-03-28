2764d67f226259458f6bfcb186f29fa60a094187
nicowave <c.nicofuentes@gmail.com>
2023-03-28 02:11:31 +0200

    i do a new components but the ccs is empty

---
+++ src/components/tweets/img-tweets.js
@@ -0,0 +1,13 @@
+import tweetsImg from "../../assets/images/Screenshot-tweets.png";
+
+import "./tweets.css";
+
+function TweetsImg() {
+  return (
+    <div className="banner-events">
+      <img src={tweetsImg} alt="events img" />
+    </div>
+  );
+}
+
+export default TweetsImg;
