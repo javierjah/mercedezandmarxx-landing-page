17bb081e006df441cbcbc335c63abfbd2be36dbc
Javier Palacios <javier.palacios.h@gmail.com>
2023-04-03 22:34:26 +0200

    make app run again after resolve conflicts

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,15 +5,11 @@ import Icon from "../../assets/images/icon_no_bg.png";
 function Header() {
   const menuItems = (
     <>
-      <li>
-        <a href="/" className="menu-item">
-          Home
-        </a>
+      <li className="menu-item">
+        <a href="/">Home</a>
       </li>
-      <li>
-        <a href="/about" className="menu-item">
-          About us
-        </a>
+      <li className="menu-item">
+        <a href="/about">About us</a>
       </li>
       <li className="menu-item">releases</li>
       <li className="menu-item">videos</li>
